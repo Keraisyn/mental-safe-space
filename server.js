518f1b7d4824b414130d04a6e09394929fc6306a
Matthews Ma <matthewsma21@gmail.com>
2020-05-31 00:00:58 -0400

    DB and score in progress

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ require("dotenv").config();
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
-mongoose.connect(process.env.DB_URI, {useNewUrlParser: true});
+mongoose.connect(process.env.DB_URI);
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, "connection error:"));
 
@@ -19,30 +19,79 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 const adminName = "Admin";
 
+const sadSchema = new mongoose.Schema({
+    name: String,
+    num: Number,    // # of messages
+    score: Number,  // Average score of last 100 messages?
+});
+
+const Sad = db.model("Sad", sadSchema);
+
 const roomSchema = new mongoose.Schema({
     contents: Array,
     name: String,
 });
 
-const Room = mongoose.model("Room", roomSchema);
+const Room = db.model("Room", roomSchema);
+
+function getScore(msg) {
+    // Calculate score with new message
+    const req = http.request({
+        host: "automl.googleapis.com",
+        path: "/v1/projects/757526254746/locations/us-central1/models/TST3342236072485584896:predict",
+        method: "POST",
+        headers: {
+            "payload": {
+                "textSnippet": {
+                    "content": msg,
+                    "mime_type": "text/plain"
+                }
+            }
+        }
+    }, (res) => {
+        let data = '';
+        res.on('data', (chunk) => {
+            data += chunk;
+        });
+        res.on('end', () => {
+            return JSON.parse(data).explanation;
+        });
+    });
+}
 
 db.once('open', function () {
 
     // Run when client connects
     io.on("connection", socket => {
+
         socket.on("joinRoom", ({username, room}) => {
             // Create mongoDB document for this room if it doesn't exist
-            new Room({name: room, contents: []}).save();
+            Room.findOne({name: room}, function (err, doc) {
+                if (err) {
+                    new Room({name: room, contents: []}).save().then(console.log("Doc created"));
+                    console.log("new room");
+                }
+                if (doc===null) {
+                    new Room({name: room, contents: []}).save().then(console.log("Doc created!!"));
+                }
+            });
+
+            // Create mongoDB document for user if it doesn't exist
+            Sad.findOne({name: username}, function (err, doc) {
+                if (err) {
+                    new Sad({name: username, num: 0, score: 0}).save();
+                }
+            });
 
             // Load previous messages from chat room
             Room.findOne({name: room}, (err, doc) => {
+                if (err) {
+                    console.log("hi");
+                    return console.error(err);
+                }
                 doc.contents.forEach((msg) => {
                    socket.emit("message", msg.formattedMessage);
                 });
-
-                if (err) {
-                    console.log(err);
-                }
             });
 
             const user = userJoin(socket.id, username, room);
@@ -77,7 +126,26 @@ db.once('open', function () {
                 if (err) {
                     console.log(err);
                 }
-            })
+            });
+
+            // Calculate new score
+            Sad.findOne({name: user.username}, function (err, doc) {
+                if (err) {
+                    console.log(err);
+                }
+                const score = getScore(msg);
+
+                const {num, currentScore} = doc;
+
+                const newScore = (currentScore * num + score) / (num + 1);
+
+                doc.score = newScore;
+                console.log(newScore);
+
+                doc.save();
+
+            });
+
         });
 
         // Runs when client disconnects
